560b0506c6a85e7042875c0989a09c30bf4e0f0c
Alex Sorokin <ilexsor@gmail.com>
2026-05-23 11:10:21 +0400

    add circular queue

--- homework/slices_and_arrays/homework_test.go
+++ homework/slices_and_arrays/homework_test.go
@@ -9,42 +9,91 @@ import (
 
 // go test -v homework_test.go
 
-type CircularQueue struct {
-	values []int
-	// need to implement
+type signedInt interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
 }
 
-func NewCircularQueue(size int) CircularQueue {
-	return CircularQueue{} // need to implement
+type circularQueue[T signedInt] struct {
+	values   []T
+	head     int
+	tail     int
+	size     int
+	capacity int
 }
 
-func (q *CircularQueue) Push(value int) bool {
-	return false // need to implement
+// NewCircularQueue create new circularQueue.
+func NewCircularQueue[T signedInt](size int) *circularQueue[T] {
+	return &circularQueue[T]{
+		values:   make([]T, size),
+		capacity: size,
+	}
 }
 
-func (q *CircularQueue) Pop() bool {
-	return false // need to implement
+// Push adds a new value to the end of the queue.
+// It returns false if the queue is full; otherwise, it inserts the value
+// and advances the tail pointer.
+func (q *circularQueue[T]) Push(value T) bool {
+	if q.Full() {
+		return false
+	}
+
+	q.values[q.tail] = value
+	q.tail = (q.tail + 1) % q.capacity
+	q.size++
+
+	return true
 }
 
-func (q *CircularQueue) Front() int {
-	return -1 // need to implement
+// Pop removes the oldest value from the front of the queue.
+// It returns false if the queue is empty; otherwise, it removes the value,
+// advances the head pointer, and returns true.
+func (q *circularQueue[T]) Pop() bool {
+	if q.Empty() {
+		return false
+	}
+	q.head = (q.head + 1) % q.capacity
+	q.size--
+
+	return true
 }
 
-func (q *CircularQueue) Back() int {
-	return -1 // need to implement
+// Front returns the value at the front of the queue without removing it.
+// If the queue is empty, it returns the -1 value of type T.
+func (q *circularQueue[T]) Front() T {
+	if q.Empty() {
+		var n T = -1
+
+		return n
+	}
+	return q.values[q.head]
 }
 
-func (q *CircularQueue) Empty() bool {
-	return false // need to implement
+// Back returns the value at the end of the queue without removing it.
+// If the queue is empty, it returns the -1 value of type T.
+func (q *circularQueue[T]) Back() T {
+	if q.Empty() {
+		var n T = -1
+
+		return n
+	}
+	lastIdx := (q.tail - 1 + q.capacity) % q.capacity
+
+	return q.values[lastIdx]
 }
 
-func (q *CircularQueue) Full() bool {
-	return false // need to implement
+// Empty reports whether the queue contains no elements.
+func (q *circularQueue[T]) Empty() bool {
+	return q.size == 0
+}
+
+// Full reports whether the queue has reached its maximum buffer capacity.
+func (q *circularQueue[T]) Full() bool {
+	return q.size == q.capacity
 }
 
 func TestCircularQueue(t *testing.T) {
 	const queueSize = 3
-	queue := NewCircularQueue(queueSize)
+	queue := NewCircularQueue[int](queueSize)
 
 	assert.True(t, queue.Empty())
 	assert.False(t, queue.Full())
