9375a4a80dc624f4e7cfce6c677f4664d4d558bd
Alex Sorokin <ilexsor@gmail.com>
2026-06-08 22:53:13 +0400

    lesson6

--- homework/structs/homework_test.go
+++ homework/structs/homework_test.go
@@ -10,83 +10,20 @@ import (
 
 type Option func(*GamePerson)
 
-func WithName(name string) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
+// GamePersonStats битовая маска для хранения всех характеристик персонажа в одном uint32
+// [31-22] - Mana (10 бит, значения 0-1000)
+// [21-12] - Health (10 бит, значения 0-1000)
+// [11-8]  - Level (4 бита, значения 0-10)
+// [7-4]   - Experience (4 бита, значения 0-10)
+// [3-0]   - Strength (4 бита, значения 0-10)
+type GamePersonStats uint32
 
-func WithCoordinates(x, y, z int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithGold(gold int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithMana(mana int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithHealth(health int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithRespect(respect int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithStrength(strength int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithExperience(experience int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithLevel(level int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithHouse() func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithGun() func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithFamily() func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
-
-func WithType(personType int) func(*GamePerson) {
-	return func(person *GamePerson) {
-		// need to implement
-	}
-}
+// GamePersonSocialStats битовая маска для хранения всех социальных характеристик персонажа в одном uint8
+// [7-4]   - Respect (4 бита, значения 0-10)
+// [3]     - HasFamily (1 бит)
+// [2]     - HasHouse (1 бит)
+// [1-0]   - Type (2 бита, значения 0-2)
+type GamePersonSocialStats uint8
 
 const (
 	BuilderGamePersonType = iota
@@ -95,88 +32,149 @@ const (
 )
 
 type GamePerson struct {
-	// need to implement
+	x           int32                 // 4 байта
+	y           int32                 // 4 байта
+	z           int32                 // 4 байта
+	gold        uint32                // 4 байта
+	stats       GamePersonStats       // 4 байта
+	name        [42]byte              // 42 байта
+	socialStats GamePersonSocialStats // 1 байт
+	hasWeapon   bool                  // 1 байт
 }
 
 func NewGamePerson(options ...Option) GamePerson {
-	// need to implement
-	return GamePerson{}
+	person := GamePerson{}
+	for _, opt := range options {
+		opt(&person)
+	}
+	return person
 }
 
 func (p *GamePerson) Name() string {
-	// need to implement
-	return ""
+	length := 0
+	for length < len(p.name) && p.name[length] != 0 {
+		length++
+	}
+	return string(p.name[:length])
 }
 
-func (p *GamePerson) X() int {
-	// need to implement
-	return 0
+func WithName(name string) Option {
+	return func(person *GamePerson) {
+		copy(person.name[:], name)
+	}
 }
 
-func (p *GamePerson) Y() int {
-	// need to implement
-	return 0
+func WithCoordinates(x, y, z int) Option {
+	return func(person *GamePerson) {
+		person.x = int32(x)
+		person.y = int32(y)
+		person.z = int32(z)
+	}
 }
 
-func (p *GamePerson) Z() int {
-	// need to implement
-	return 0
+func WithGold(gold int) Option {
+	return func(person *GamePerson) {
+		person.gold = uint32(gold)
+	}
 }
 
-func (p *GamePerson) Gold() int {
-	// need to implement
-	return 0
+func WithMana(mana int) Option {
+	return func(person *GamePerson) {
+		// Mana: биты 22-31 (10 бит)
+		// Очищаем последние 10 бит 0xFFC00000 в двоичном виде: 11111111110000000000000000000000 (биты 22-31)
+		// Если в маске (в числе справа) стоит 1, то в исходном числе этот бит принудительно сбрасывается в 0.
+		person.stats &^= 0xFFC00000
+		// 0x3FF = 0000 0000 0000 0000 0000 0011 1111 1111Оператор & (битовое И) работает по правилу:
+		// результат равен 1 только тогда, когда оба бита равны 1. Если один из битов 0, на выходе будет 0.
+		// mana&0x3FF это трафарет для защиты от переполнения
+		person.stats |= GamePersonStats(mana&0x3FF) << 22
+	}
 }
 
-func (p *GamePerson) Mana() int {
-	// need to implement
-	return 0
+func WithHealth(health int) Option {
+	return func(person *GamePerson) {
+		// Health: биты 12-21 (10 бит)
+		person.stats &^= 0x003FF000
+		person.stats |= GamePersonStats(health&0x3FF) << 12
+	}
 }
 
-func (p *GamePerson) Health() int {
-	// need to implement
-	return 0
+func WithRespect(respect int) Option {
+	return func(person *GamePerson) {
+		// Respect: биты 4-7 (4 бита)
+		person.socialStats &^= 0xF0
+		person.socialStats |= GamePersonSocialStats(respect&0x0F) << 4
+	}
 }
 
-func (p *GamePerson) Respect() int {
-	// need to implement
-	return 0
+func WithStrength(strength int) Option {
+	return func(person *GamePerson) {
+		// Strength: биты 0-3 (4 бита)
+		person.stats &^= 0x0000000F
+		person.stats |= GamePersonStats(strength & 0x0F)
+	}
 }
 
-func (p *GamePerson) Strength() int {
-	// need to implement
-	return 0
+func WithExperience(experience int) Option {
+	return func(person *GamePerson) {
+		// Experience: биты 4-7 (4 бита)
+		person.stats &^= 0x000000F0
+		person.stats |= GamePersonStats(experience&0x0F) << 4
+	}
 }
 
-func (p *GamePerson) Experience() int {
-	// need to implement
-	return 0
+func WithLevel(level int) Option {
+	return func(person *GamePerson) {
+		// Level: биты 8-11 (4 бита)
+		person.stats &^= 0x00000F00
+		person.stats |= GamePersonStats(level&0x0F) << 8
+	}
 }
 
-func (p *GamePerson) Level() int {
-	// need to implement
-	return 0
+func WithHouse() Option {
+	return func(person *GamePerson) {
+		person.socialStats |= 0x04 // бит 2 для HasHouse
+	}
 }
 
-func (p *GamePerson) HasHouse() bool {
-	// need to implement
-	return false
+func WithGun() Option {
+	return func(person *GamePerson) {
+		person.hasWeapon = true
+	}
 }
 
-func (p *GamePerson) HasGun() bool {
-	// need to implement
-	return false
+func WithFamily() Option {
+	return func(person *GamePerson) {
+		person.socialStats |= 0x08 // бит 3 для HasFamily
+	}
 }
 
-func (p *GamePerson) HasFamilty() bool {
-	// need to implement
-	return false
+func WithType(personType int) Option {
+	return func(person *GamePerson) {
+		// Type: биты 0-1 (2 бита)
+		person.socialStats &^= 0x03
+		person.socialStats |= GamePersonSocialStats(personType & 0x03)
+	}
 }
 
-func (p *GamePerson) Type() int {
-	// need to implement
-	return 0
-}
+func (p *GamePerson) X() int    { return int(p.x) }
+func (p *GamePerson) Y() int    { return int(p.y) }
+func (p *GamePerson) Z() int    { return int(p.z) }
+func (p *GamePerson) Gold() int { return int(p.gold) }
+
+// Обратная операция по чтению бит с 31 по 22
+// Сдвигаем все биты с 31 по 22 в начало, остальные биты зануляются
+// И для безопасности применяем маску 0x3FF на получившееся число
+func (p *GamePerson) Mana() int        { return int((p.stats >> 22) & 0x3FF) }
+func (p *GamePerson) Health() int      { return int((p.stats >> 12) & 0x3FF) }
+func (p *GamePerson) Respect() int     { return int((p.socialStats >> 4) & 0x0F) }
+func (p *GamePerson) Strength() int    { return int(p.stats & 0x0F) }
+func (p *GamePerson) Experience() int  { return int((p.stats >> 4) & 0x0F) }
+func (p *GamePerson) Level() int       { return int((p.stats >> 8) & 0x0F) }
+func (p *GamePerson) HasHouse() bool   { return p.socialStats&0x04 != 0 }
+func (p *GamePerson) HasGun() bool     { return p.hasWeapon }
+func (p *GamePerson) HasFamilty() bool { return p.socialStats&0x08 != 0 }
+func (p *GamePerson) Type() int        { return int(p.socialStats & 0x03) }
 
 func TestGamePerson(t *testing.T) {
 	assert.LessOrEqual(t, unsafe.Sizeof(GamePerson{}), uintptr(64))
