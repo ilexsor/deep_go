b3de714af5479fb09d307b727bd3a18e7b08303d
Alex Sorokin <ilexsor@gmail.com>
2026-06-18 20:17:17 +0400

    lesson9

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +18,71 @@ type Person struct {
 	Married bool   `properties:"married"`
 }
 
-func Serialize(person Person) string {
-	// need to implement
-	return ""
+func Serialize(v any) string {
+	val := reflect.ValueOf(v)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	typ := val.Type()
+	var lines []string
+
+	for i := 0; i < val.NumField(); i++ {
+		fieldVal := val.Field(i)
+		fieldType := typ.Field(i)
+
+		if !fieldType.IsExported() {
+			continue
+		}
+
+		tag := fieldType.Tag.Get("properties")
+		if tag == "" || tag == "-" {
+			continue
+		}
+
+		tagParts := strings.Split(tag, ",")
+		key := strings.TrimSpace(tagParts[0])
+
+		omitempty := false
+		if len(tagParts) > 1 && strings.TrimSpace(tagParts[1]) == "omitempty" {
+			omitempty = true
+		}
+
+		if omitempty && fieldVal.IsZero() {
+			continue
+		}
+
+		var strVal string
+		switch fieldVal.Kind() {
+		case reflect.String:
+			strVal = fieldVal.String()
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			strVal = fmt.Sprintf("%d", fieldVal.Int())
+		case reflect.Bool:
+			strVal = fmt.Sprintf("%t", fieldVal.Bool())
+		case reflect.Float32, reflect.Float64:
+			strVal = fmt.Sprintf("%g", fieldVal.Float())
+		default:
+			strVal = fmt.Sprintf("%v", fieldVal.Interface())
+		}
+
+		key = escapeKey(key)
+
+		lines = append(lines, fmt.Sprintf("%s=%s", key, strVal))
+	}
+
+	return strings.Join(lines, "\n")
+}
+
+// escapeKey экранирует служебные символы в ключах
+func escapeKey(key string) string {
+	r := strings.NewReplacer(
+		" ", "\\ ",
+		"=", "\\=",
+		":", "\\:",
+	)
+	return r.Replace(key)
 }
 
 func TestSerialization(t *testing.T) {
